013955ffbabb635776e7ee7144bc19af18d952b1
OOASE <89398875+OOASE@users.noreply.github.com>
2021-08-28 07:47:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+
+# k = '''plug logistics is the best logistics compny'''
+# print(k)
+
+# G = """Godwin is my friend 
+# of about 5 years"""
+# print(G)
+
+# T ="""Tolu is a good guy"""
+# print(T)
+
+# print(T[1:3])
+# print(T[6])
+
+# T[2] = 'is'
+# print(T2)
+
+# x = range(10)
+# for n in x:
+#   print(n)
+
+# dict = {'name': 'kunle','shoesize' :42}
+# print(type(dict))
+# print(dict)
+# print(dict.get('name'))
+# print(dict['shoesize'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
